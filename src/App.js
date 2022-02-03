23c56da3668e0a887cf5efff4b40142a84cf260c
Joseane-Guedes <joseane_23@hotmail.com>
2022-02-03 15:46:57 -0300

    my first component list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-const App = ( ) => {
-  return <p>Hello react!</p>;
+import NotesList from "./components/NotesList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <NotesList />
+    </div>
+  );
 };
 
 export default App;
