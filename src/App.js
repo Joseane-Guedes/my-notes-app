6540b5f5c388fe30535554b3d135938ab85cf2c5
Joseane-Guedes <joseane_23@hotmail.com>
2022-02-03 22:23:12 -0300

    Add noteprops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState} from 'react';
 import NotesList from "./components/NotesList";
+import  {nanoid} from 'nanoid';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+        id: nanoid(),
+        text: "My first note!",
+        date: "03/02/2021"
+  },
+  {
+    id: nanoid(),
+    text: "My second note!",
+    date: "03/02/2021"
+},
+{
+  id: nanoid(),
+  text: "My third note!",
+  date: "03/02/2021"
+},
+{
+  id: nanoid(),
+  text: "My new note!",
+  date: "03/02/2021"
+}
+]) ;
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
